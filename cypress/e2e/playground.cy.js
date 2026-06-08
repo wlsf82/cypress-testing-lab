a9dde62af30bc28a7e72ac6f7f5b9c538034c8c9
Walmyr <wlsf82@gmail.com>
2026-06-08 18:03:21 +0200

    Add test for newsletter subscription

--- cypress/e2e/playground.cy.js
+++ cypress/e2e/playground.cy.js
@@ -3,8 +3,12 @@ describe('Cypress Playground', () => {
     cy.visit('/index.html')
   })
 
-  it('asserts the subscribe button is visible using `cy.get` and `cy.contains`', () => {
-    cy.get('button[type="submit"]').should('be.visible')
-    cy.contains('button', 'Subscribe').should('be.visible')
+  it('successfully subscribes to the newsletter', () => {
+    cy.contains('button', 'Subscribe').click()
+
+    cy.contains(
+      'span#success',
+      "You've been successfully subscribed to our newsletter."
+    ).should('be.visible')
   })
 })
